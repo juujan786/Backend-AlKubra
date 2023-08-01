2c6e17a1b022959af2e9877baea2a3f0de3d2f5b
juujan786 <jhsurmovi@gmail.com>
2023-08-01 15:08:15 +0500

    payment method integrated

--- backend/app.js
+++ backend/app.js
@@ -2,19 +2,36 @@ const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
 const errorMiddleware = require("./middleware/error");
+const cors = require("cors");
 
 app.use(express.json());
 app.use(cookieParser());
+
+//set up cors
+const allowedOrigin = ["http://localhost:3000"];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigin.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not Allowed by CORS"));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
 
 // Route Imports
 const product = require("./routes/productRoutes");
 const user = require("./routes/userRoute");
 const order = require("./routes/orderRoute");
+const payment = require("./routes/paymentRoutes");
 
- 
 app.use("/api/v1", product);
 app.use("/api/v1", user);
 app.use("/api/v1", order);
+app.use("/api/v1", payment);
 
 // Middleware for Errors
 app.use(errorMiddleware);
